a5bea5525d2c2010d66f2d2406131ae2701bb139
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-07-29 14:58:20 -0400

    first commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,142 @@
 class Article:
+    articles = []
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.articles.append(self)
+    def __repr__(self):
+        return f"Article(author={self.author}, magazine={self.magazine}, title={self.title})"
+
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author (self, value):
+        if isinstance(value, Author):
+            self._author = value
+        else:
+            raise Exception("author must be of type Author")
+        
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine (self, value):
+        if isinstance(value, Magazine):
+            self._magazine = value
+        else:
+            raise Exception("magazine must be of type Magazine")
+            
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and 5 <= len(value) <= 50 and not hasattr(self, "_title"):
+            self._title = value
+        else:
+            raise Exception("Title must be a string greater than 5 and less than 50 inclusive")
         
 class Author:
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+        return f"Author(name={self.name})"
+
     def articles(self):
-        pass
+        return[article.title for article in Article.articles if article.author == self]
+        
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in Article.articles if article.author == self]))
 
     def add_article(self, magazine, title):
-        pass
+        ##Article is author, magazine, title
+        ##magazine is name and category
+        ##would have to make magazine instance?
+        return Article(self, magazine, title)
+
+        
 
     def topic_areas(self):
-        pass
+        return list(set([article.magazine.category for article in Article.articles if article.author == self]))
+        
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0 and not hasattr(self, '_name'):
+            self._name = value
+        else:
+            raise Exception("name must be a string value greater than 0")
+
+
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+    
+    def __repr__(self):
+        return f"Magazine(name={self.name}, category={self.category})"
 
     def articles(self):
-        pass
+        return [article for article in Article.articles if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in Article.articles if article.magazine == self))
 
     def article_titles(self):
-        pass
+        if len(Article.articles)>0:
+            return [article.title for article in Article.articles if article.magazine == self]
+        else:
+            return None
+        
 
     def contributing_authors(self):
-        pass
+        authors = list(set(article.author for article in Article.articles if article.magazine == self))
+        contributing_authors = []
+        for author in authors:
+            if len([article for article in Article.articles if article.magazine == self and article.author == author]) > 2:
+                contributing_authors.append(author)
+        if len(contributing_authors) > 0:
+            return contributing_authors
+        else:
+            return None
+
+    def top_publisher(self):
+        articles = list(article.author.name for article in Article.articles if article.magazine == self)
+        return max(articles)
+                
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+        else:
+            raise Exception("Name must be a string greater than 2 character and less than 16 characters inclusive")
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+        else:
+            raise Exception("Category must be a string greater than 0")
--- lib/debug.py
+++ lib/debug.py
@@ -8,6 +8,44 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    # Creating Authors
+    author1 = Author("Pedro")
+    author2 = Author("Tay")
+    author3 = Author("mini")
+    author4 = Author("andrew")
+    author5 = Author("sasha")
 
-    # don't remove this line, it's for debugging!
+    # Creating Magazines
+    magazine1 = Magazine("Conde", "Fly")
+    magazine2 = Magazine("MM", "Super Fly")
+    magazine3 = Magazine("Prada", "Fashion")
+    magazine4 = Magazine("Tech Today", "Technology")
+    magazine5 = Magazine("Foodie", "Culinary")
+
+    # Creating Articles
+    article1 = Article(author1, magazine1, "The December Issue")
+    article2 = Article(author2, magazine2, "October Festivities")
+    article3 = Article(author2, magazine3, "May Flowers")
+    article4 = Article(author2, magazine3, "June Blossoms")
+    article5 = Article(author1, magazine3, "November Rain")
+    article6 = Article(author3, magazine4, "Tech Innovations")
+    article7 = Article(author3, magazine4, "AI in 2024")
+    article8 = Article(author4, magazine5, "Gourmet Cooking")
+    article9 = Article(author4, magazine5, "Desserts Galore")
+    article10 = Article(author5, magazine1, "Travel Tips")
+
+    # Additional Articles for Testing Contributing Authors
+    article11 = Article(author1, magazine3, "Winter Wonderland")
+    article12 = Article(author1, magazine3, "Spring Awakening")
+
+    # Test some more combinations
+    article13 = Article(author5, magazine2, "The New Wave")
+    article14 = Article(author5, magazine2, "Autumn Leaves")
+    article15 = Article(author5, magazine2, "Summer Breeze")
+
+
+    
+    
+    
+    # # don't remove this line, it's for debugging!
     ipdb.set_trace()
